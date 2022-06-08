2559ed04748fa0ebef76df89e841c9f183a76b23
juliusw01 <95189236+juliusw01@users.noreply.github.com>
2022-06-08 15:37:14 +0200

    INIT

---
+++ src/main/java/Ingo/Instrumentenverleih/InstrumentenverleihApplication.java
@@ -0,0 +1,13 @@
+package Ingo.Instrumentenverleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentenverleihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstrumentenverleihApplication.class, args);
+	}
+
+}
